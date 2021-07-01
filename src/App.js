e27ef526097661d48db44fb11a2c73b8c60e136d
ColeNicholson <nicholsc@sonoma.edu>
2021-07-01 12:02:42 -0700

    Implemented Search Bar Framework

--- src/App.js
+++ src/App.js
@@ -1,48 +1,68 @@
 import './App.css';
-import {React, Component} from 'react'
+import React from 'react'
+import Button from 'react-bootstrap/Button'
 import api from './api'
 import apiResp from './apiResp'
+import {Container} from "react-bootstrap";
 
-class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false, 
-      ...apiResp.createMangaResponse()
+function SearchBar(){
+    const [searchQuery, setSearchQuery] = React.useState();
+
+    const handleChange = e => {
+        setSearchQuery(e.target)
     }
-  }
 
-  componentDidMount(){
-    api.getRandomManga()
-    .then((response) => {
-      console.log(response.data)
-      this.setState({
-        isLoaded: true,
-        respID: response.data.data.id,
-        respType: response.data.data.type,
-        respAttr: response.data.data.attributes
-      });
-    })
-    .catch((error) => {
-      console.log(error)
-      this.setState({
-        isLoaded: false,
-        error
-      });
-    })
+    const handleInput = e => {
+
+        setSearchQuery('')
+    }
+
+    return(
+        <div>
+            <input size={60} type='text' placeholder='Find a Manga!' onChange={handleChange}/>
+            <Button onClick={handleInput}>Search</Button>
+        </div>
+    );
+}
+
+function App() {
+  
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isLoaded: false,
+  //     ...apiResp.createMangaResponse()
+  //   }
+  // }
+  //
+  // componentDidMount(){
+  //   api.getRandomManga()
+  //   .then((response) => {
+  //     console.log(response.data)
+  //     this.setState({
+  //       isLoaded: true,
+  //       respID: response.data.data.id,
+  //       respType: response.data.data.type,
+  //       respAttr: response.data.data.attributes
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error)
+  //     this.setState({
+  //       isLoaded: false,
+  //       error
+  //     });
+  //   })
     
-  }
+  //}
 
-  render() {
     return (
       <div>
-        <h1>{this.state.respAttr.title.en}</h1>
-        <p>{this.state.respAttr.description.en}</p>
+          <Container>
+            <SearchBar></SearchBar>
+          </Container>
       </div>
-    )
-  }
-
-}
+    );
+};
  
 export default App;
---
+++ src/testfile.js
@@ -0,0 +1,48 @@
+import './App.css';
+import {React, Component} from 'react'
+import api from './api'
+import apiResp from './apiResp'
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            ...apiResp.createMangaResponse()
+        }
+    }
+
+    componentDidMount(){
+        api.getRandomManga()
+            .then((response) => {
+                console.log(response.data)
+                this.setState({
+                    isLoaded: true,
+                    respID: response.data.data.id,
+                    respType: response.data.data.type,
+                    respAttr: response.data.data.attributes
+                });
+            })
+            .catch((error) => {
+                console.log(error)
+                this.setState({
+                    isLoaded: false,
+                    error
+                });
+            })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.respAttr.title.en}</h1>
+                <p>{this.state.respAttr.description.en}</p>
+            </div>
+        )
+    }
+
+}
+
+export default App;
