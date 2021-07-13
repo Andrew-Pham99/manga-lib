7c515ece2779f0afbb06d673fd718b7be66501cf
ColeNicholson <nicholsc@sonoma.edu>
2021-07-12 17:20:13 -0700

    Template for manga result cards + placeholder files

--- src/App.js
+++ src/App.js
@@ -8,13 +8,28 @@ import apiResp from './apiResp'
 import {Container} from "react-bootstrap";
 import Grid from '@material-ui/core/Grid'
 
+// Remove this when you no longer need a placeholder image
+import placeholderImage from "./images/Broda.jpg"
+
 
 function MangaCard(props){
+    // For this to work you need to add 2 props: description and img from the api
+    // I can add onClick functions to handle the view transformation with you want to keep everything
+    //  on the same page, or we can have the button route to a new page.
     return(
         <Card style={{width: '25rem'}}>
-        <Card.Body>
-            <Card.Title>{props.name}</Card.Title>
-        </Card.Body>
+            <Card.Img variant={"top"} src={props.img} alt={"No Image Found"} className={"thumbnail"} width={"350px"}/>
+            <Card.Body>
+                <Card.Title>
+                    <h1>{props.name}</h1>
+                </Card.Title>
+                <Card.Text>
+                    <p>{props.description}</p>
+                </Card.Text>
+            </Card.Body>
+            <Button variant={"primary"}>
+                Read {props.name}
+            </Button>
         </Card>
     );
 }
@@ -99,14 +114,16 @@ function SearchBar(props){
             />
             <ul>
                 <Grid container className="recipe-space" spacing={2}>
-                    <Grid item md={12}>
-                        <Grid container justify="center" spacing={.05}>
-            {responseData.map((item,index) =>(
-                <MangaCard
-                    key={index}
-                    name={item.data.attributes.title.en}
-                />
-                ))}
+                    <Grid item md={8}>
+                        <Grid container justify="center" spacing={1}>
+                            {responseData.map((item,index) =>(
+                            <MangaCard
+                                key={index}
+                                name={item.data.attributes.title.en}
+                                img={placeholderImage}
+                                description={"This is a placeholder description"}
+                            />
+                            ))}
                         </Grid>
                     </Grid>
                 </Grid>
