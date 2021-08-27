c8066f97f05bf8f445ccd97a90be8093cc85b57e
ColeNicholson <nicholsc@sonoma.edu>
2021-08-27 02:25:22 -0700

    Getting cucked by asynchronicity again!

--- src/Reader.js
+++ src/Reader.js
@@ -8,12 +8,22 @@ import {Navbar, Nav, Container} from "react-bootstrap";
 function ChapterListNav() {
     const [context, setContext] = React.useState(useLocation());
     const [chapterList, setChapterList] = React.useState([]);
+    const [chapterImgUrlList, setChapterImgUrlList] = React.useState([]);
     const getChapterList = () => {
         console.log("xd")
         api.getChapterList({manga:context.state.id})
             .then((response) => {
-                setChapterList(response.data.results)
+                console.log(response)
+                setChapterList(chapterList => response.data.results); // Need this to be synchronously set so we can grab the img urls in the next .then block
                 console.log(chapterList)
+                return api.getBaseUrl(response.data.results[0].data.id) // I think we only need to call this for one chapter and it will work for all
+            })
+            .then((response) => {
+                chapterList[0].data.attributes.data.forEach(chapterImg => {
+                    //console.log("url is " + response.data.baseUrl + '/data/' + chapter.data.attributes.hash + '/' + chapterImg);
+                    setChapterImgUrlList(chapterImgUrlList => [...chapterImgUrlList, api.getChapterImgUrl(response.data.baseUrl, 'data', chapterList[0].data.attributes.hash, chapterImg)])
+                    console.log(chapterImgUrlList)
+                })
             })
             .catch((error) => {
                 console.log(error)
@@ -21,13 +31,14 @@ function ChapterListNav() {
     }
     React.useEffect(() => getChapterList(), []); // This function can be used to make things only happen once!
 
+
     return (
         // Do we want to link to different pages on each item in the nav, or do we want to spawn the chapter here?
         <div>
             <Navbar>
                 <Nav className={"flex-column"}>
-                {chapterList.map((chapter) => (
-                    <Nav className={"flex-column"}>Chapter {chapter.data.attributes.chapter}</Nav>
+                {chapterList.map((chapter, index) => (
+                    <Nav className={"flex-column"} key={index}>Chapter {chapter.data.attributes.chapter}</Nav>
                 ))}
                 </Nav>
             </Navbar>
@@ -43,7 +54,7 @@ function Reader(props) {
         <div className="Reader">
             <Container>
                 <h1>{context.state.name}</h1>
-                <img src={context.state.img} width={250} />
+                <img src={context.state.img} width={250} alt={"Not Found"} />
                 <ChapterListNav/>
             </Container>
         </div>
