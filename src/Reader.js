cfa2ef24c3c5e0cdd11e2b314b4d3416340f38b7
ColeNicholson <nicholsc@sonoma.edu>
2021-09-03 16:56:54 -0700

    Moved chapter list tech to its own function

--- src/Reader.js
+++ src/Reader.js
@@ -21,27 +21,12 @@ function ChapterImages() {
                 console.log(error)
             })
     }
-    const HandleChapterChange = newChapter => {
-        history.push({pathname:`/Reader/manga=${context.state.manga.id}/chapter=${newChapter.data.attributes.chapter}`, state:{manga:context.state.manga, curChapter:newChapter, chapterList:context.state.chapterList}});
-    }
     React.useEffect(() => {getChapterImages(context.state.curChapter.data.id);}, [context])
     // React.useEffect(() => {console.log(context);}, [context])
     // React.useEffect(() => {console.log(history);}, [history])
 
     return (
         <div>
-            <Navbar className="ChapterList">
-                <Nav className={"flex-column"}>
-                    {context.state.chapterList.map((chapter, index) => (
-                        <Nav.Item key={index} onClick={()=>HandleChapterChange(chapter)}>
-                            <Nav.Link>
-                                {chapter.data.attributes.title !== "" ? `Chapter ${chapter.data.attributes.chapter} - ${chapter.data.attributes.title}` :
-                                    `Chapter ${chapter.data.attributes.chapter}`}
-                            </Nav.Link>
-                        </Nav.Item>
-                    ))}
-                </Nav>
-            </Navbar>
             <Container>
                 {chapterImgUrlList.map((chapterImg, index) => (
                     <Image src={chapterImg} key={index} alt={"Not Found"}></Image>
@@ -52,18 +37,22 @@ function ChapterImages() {
 }
 
 function ChapterListHamburgerMenu() { // Make the chapter list hamburger menu in here
-    const [context, setContext] = React.useState(useLocation());
+    const context =useLocation();
+    const [history, setHistory] = React.useState(useHistory());
+
+    const HandleChapterChange = newChapter => {
+        history.push({pathname:`/Reader/manga=${context.state.manga.id}/chapter=${newChapter.data.attributes.chapter}`, state:{manga:context.state.manga, curChapter:newChapter, chapterList:context.state.chapterList}});
+    }
+
     return (
         <div>
             <Navbar  className="ChapterList">
                 <Nav className={"flex-column"}>
                     {context.state.chapterList.map((chapter, index) => (
-                        <Nav.Item key={index}>
+                        <Nav.Item key={index} onClick={()=>HandleChapterChange(chapter)}>
                             <Nav.Link>
-                                <Link className="chapter" to={{pathname:`/Reader/manga=${context.state.manga.id}/chapter=${chapter.data.attributes.chapter}`, state:{manga:context.state, curChapter:chapter, chapterList:context.state.chapterList}}}>
                                     {chapter.data.attributes.title !== "" ? `Chapter ${chapter.data.attributes.chapter} - ${chapter.data.attributes.title}` :
                                         `Chapter ${chapter.data.attributes.chapter}`}
-                                </Link>
                             </Nav.Link>
                         </Nav.Item>
                     ))}
@@ -81,6 +70,7 @@ function Reader() {
             <components.TopNavBar/>
             <Container>
                 <h1>You are reading {context.state.manga.name} Chapter {context.state.curChapter.data.attributes.chapter}</h1>
+                <ChapterListHamburgerMenu/>
                 <ChapterImages/>
             </Container>
         </div>
