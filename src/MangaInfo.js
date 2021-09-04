38b57fcb6675e37e2a7e0cdc4bd1de060868f99f
ColeNicholson <nicholsc@sonoma.edu>
2021-09-03 19:43:49 -0700

    Modified chapter list to include listId on each element for use in Reader.js

--- src/MangaInfo.js
+++ src/MangaInfo.js
@@ -37,8 +37,10 @@ function ChapterListNav() {
         setChapterList([])
         api.getChapterList({manga: context.state.id})
             .then((getChapterListResponse) => {
+                getChapterListResponse.data.results.forEach((chapter, index) => {
+                    setChapterList(chapterList => [...chapterList, {data:chapter.data, relationships: chapter.relationships, result:chapter.result, listId:index}])
+                })
                 console.log(getChapterListResponse)
-                setChapterList(chapterList => getChapterListResponse.data.results);
                 setPageLength(Math.ceil(getChapterListResponse.data.total/api.ch_limit))
                 if(Math.ceil(getChapterListResponse.data.total/api.ch_limit) > 1)
                 {
@@ -50,7 +52,7 @@ function ChapterListNav() {
             })
     }
     React.useEffect(() => {getChapterList();}, []);
-    React.useEffect(() => console.log(chapterList), [chapterList]) // Logs every time the chapter list updates, remove if annoying
+    //React.useEffect(() => console.log(chapterList), [chapterList]) // Logs every time the chapter list updates, remove if annoying
     
 
     const handlePageClick = (e) => {
@@ -90,7 +92,8 @@ function ChapterListNav() {
                 {chapterList.map((chapter, index) => (
                     <Nav.Item key={index}>
                         <Nav.Link>
-                            <Link className="chapter" to={{pathname:`/Reader/manga=${context.state.id}/chapter=${chapter.data.attributes.chapter}`, state:{manga:context.state, curChapter:chapter, chapterList:chapterList}}}>
+                            <Link className="chapter" to={{pathname:`/Reader/manga=${context.state.id}/chapter=${chapter.data.attributes.chapter}`,
+                                state:{manga:context.state, curChapter:chapter, chapterList:chapterList}}}>
                                 {chapter.data.attributes.title !== "" ? `Chapter ${chapter.data.attributes.chapter} - ${chapter.data.attributes.title}` :
                                     `Chapter ${chapter.data.attributes.chapter}`}
                             </Link>
