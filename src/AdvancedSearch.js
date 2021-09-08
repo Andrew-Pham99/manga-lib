d8d013c463a1603840c1aa6f637a5b770df4ad87
ColeNicholson <nicholsc@sonoma.edu>
2021-09-08 15:42:18 -0700

    Added Advanced Search skeleton

---
+++ src/AdvancedSearch.js
@@ -0,0 +1,15 @@
+import React from "react";
+import api from "./api";
+import components from "./components/components";
+import {useLocation, useHistory, Link} from "react-router-bootstrap";
+import "./AdvancedSearch.css"
+
+function AdvancedSearch() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default AdvancedSearch;
