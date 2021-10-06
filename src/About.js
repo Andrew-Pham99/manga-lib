ec593c126bc7a6c0802f7cc4bbb9607e1eb4333c
ColeNicholson <nicholsc@sonoma.edu>
2021-10-06 16:18:46 -0700

    Skeleton for AboutUs page

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+// TODO : Put about us information here and a way to route to this page
+//      Probably want contact info, what this is, why we made it, etc...
+function About() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default About;
