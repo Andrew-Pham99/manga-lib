5a321711aff032831bc6be0ddd453ec284d9e88a
ColeNicholson <nicholsc@sonoma.edu>
2021-10-09 02:42:02 -0700

    styled not found page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import components from "./components/components";
+import "./css/css_globals.css"
 
 export default function NotFound() {
     return (
         <div align={"center"}>
             <components.TopNavBar/>
-            <h3>Sorry, this page could not be found!</h3>
+            <h3 style={{color:"var(--text-color)", marginTop:20}}>Sorry, this page could not be found!</h3>
         </div>
     )
 }
